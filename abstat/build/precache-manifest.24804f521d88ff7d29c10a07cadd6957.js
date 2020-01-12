be830caf925e03ac58550cb15be722e9f8b2c0c4
Dave <davebrown211@gmail.com>
2020-01-12 15:49:27 -0600

    updates

---
+++ abstat/build/precache-manifest.24804f521d88ff7d29c10a07cadd6957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f7480f6d12511251c74dfc3e80fadd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cd836767799ec9b623a",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "d3a7ab998806a5ecc63e",
+    "url": "/static/js/2.f976a26b.chunk.js"
+  },
+  {
+    "revision": "4cd836767799ec9b623a",
+    "url": "/static/js/main.6d0e8ff4.chunk.js"
+  },
+  {
+    "revision": "0c7281d4150a134cce25",
+    "url": "/static/js/runtime-main.a2f9a6be.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
