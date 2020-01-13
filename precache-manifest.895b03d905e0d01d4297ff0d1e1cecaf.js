df7daf051848ce482c669abf6cf2ccef73815190
dave <davebrown211@gmail.com>
2020-01-13 13:29:16 -0600

    Updates

---
+++ precache-manifest.895b03d905e0d01d4297ff0d1e1cecaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceee716b8ca67c53c694bba8c4200489",
+    "url": "/abstat-web/index.html"
+  },
+  {
+    "revision": "e9a9c58d6875a74634c8",
+    "url": "/abstat-web/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d3a7ab998806a5ecc63e",
+    "url": "/abstat-web/static/js/2.f976a26b.chunk.js"
+  },
+  {
+    "revision": "e9a9c58d6875a74634c8",
+    "url": "/abstat-web/static/js/main.ab233a18.chunk.js"
+  },
+  {
+    "revision": "60453f555d7fe8559adb",
+    "url": "/abstat-web/static/js/runtime-main.f480f844.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/abstat-web/static/media/logo.25bf045c.svg"
+  }
+]);
